93d712708adce816929c775c8d57a90b33aaa0c0
vietman2 <vietman0@gmail.com>
2023-10-02 12:35:24 +0900

    user model plan

--- user_management_service/user/models.py
+++ user_management_service/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class CustomUser(models.Model):
+    # 관리자 (우리)
+    # 일반유저 (일반유저 + 게스트)
+    # 코치 (개인레슨코치 + 시설고용코치)
+    # 시설업자
+    # 파트너 (광고주 + 제휴업체)
+    # 상담원
+
+    #class UserType(models.TextChoices):
+    #    ADMIN = "admin", "관리자"
+    #    NORMAL = "normal", "일반유저"
+    #    COACH = "coach", "코치"
+    #    FACILITY_OWNER = "ownwer", "시설업자"
+
+    ## 아래는 placeholder
+    username = models.CharField(max_length=50)
