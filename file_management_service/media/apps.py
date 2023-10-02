b705d89fa778ff73ea8b49b6e8e09554a03333f3
vietman2 <vietman0@gmail.com>
2023-10-02 11:51:16 +0900

    refactor: merge photo and video into one app

--- file_management_service/media/apps.py
+++ file_management_service/media/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class VideoConfig(AppConfig):
+class MediaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'video'
+    name = 'media'
