d319d2d72198ee4ab4d96fda22231e34f90b81d3
vietman2 <vietman0@gmail.com>
2023-10-09 14:44:19 +0900

    link urls for user module and auto schema

--- user_management_service/user_management_service/urls.py
+++ user_management_service/user_management_service/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView as SSV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/account/', include('user.urls')),
+
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SSV.as_view(url_name='schema'), name='swagger-ui')
 ]
