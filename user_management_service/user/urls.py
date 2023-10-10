895f04eb82a1cbfc5f6b623842559e81f076001b
vietman2 <vietman0@gmail.com>
2023-10-10 14:22:36 +0900

    add urls for each API

--- user_management_service/user/urls.py
+++ user_management_service/user/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path, include
+from django.urls import path
+from dj_rest_auth.views import (
+    LoginView, LogoutView, PasswordChangeView,
+    PasswordResetConfirmView, PasswordResetView
+)
 
 from .views import RegisterAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
-    # path("api-auth/", include()"rest_framework.urls", namespace="rest_framework")),
-    # path("change-password/", )
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("password-reset/", PasswordResetView.as_view(), name="password-reset"),
+    path("password-reset-confirm/", PasswordResetConfirmView.as_view(), name="password-reset-confirm"),
+    path("password-change/", PasswordChangeView.as_view(), name="password-change"),
     # path("user/", )
-
 ]
