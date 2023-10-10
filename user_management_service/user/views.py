407443da1f835b67301675fd3ed0681e4040e3af
vietman2 <vietman0@gmail.com>
2023-10-10 14:23:03 +0900

    add second pw validation and fix error handling

--- user_management_service/user/views.py
+++ user_management_service/user/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
-from django.core.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError
 
 from .serializers import UserRegisterSerializer
 
@@ -9,15 +9,17 @@ class RegisterAPIView(CreateAPIView):
     serializer_class = UserRegisterSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = UserRegisterSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
 
         try:
             serializer.is_valid(raise_exception=True)
-            super().post(request, *args, **kwargs)
-            return Response(data={
-                "message": "회원가입이 완료되었습니다.",
-            }, status=status.HTTP_201_CREATED)
+            serializer.validate_passwords(serializer.validated_data)
         except ValidationError as e:
             return Response(data={
                 "errors": e.detail,
             }, status=status.HTTP_400_BAD_REQUEST)
+
+        super().post(request, *args, **kwargs)
+        return Response(data={
+            "message": "회원가입이 완료되었습니다.",
+        }, status=status.HTTP_201_CREATED)
