5ac8731d1e6bb536f34f396eeaabb4830b20f1f6
vietman2 <vietman0@gmail.com>
2023-10-10 14:23:38 +0900

    fix serializer

--- user_management_service/user/serializers.py
+++ user_management_service/user/serializers.py
@@ -2,29 +2,32 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
-from phonenumber_field.serializerfields import PhoneNumberField
 
 from .models import CustomUser
 
 class UserRegisterSerializer(ModelSerializer):
-    phone_number = PhoneNumberField(required=True, allow_null=False, allow_blank=False, validators=[UniqueValidator(queryset=CustomUser.objects.all())])
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    username = serializers.CharField(required=True, allow_null=False, allow_blank=False, validators=[UniqueValidator(queryset=CustomUser.objects.all())])
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = [
-            "username",
-            "password",
-            "first_name",
-            "last_name",
-            "email",
-            "phone_number",
-            "birth_date",
-            "gender",
-            "experience_tier",
-        ]
+        fields = ["username", "first_name", "last_name", "email", "phone_number", "password", "password2"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "password2": {"write_only": True},
+            "username": {"validators": [UniqueValidator(queryset=CustomUser.objects.all())]},
+            "phone_number": {"validators": [UniqueValidator(queryset=CustomUser.objects.all())]},
+        }
     
+    def validate_passwords(self, value):
+        if value["password"] != value["password2"]:
+            raise serializers.ValidationError("비밀번호가 일치하지 않습니다.")
+        return value
+    
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
     def create(self, validated_data):
+        validated_data.pop("password2")
         user = CustomUser.objects.create_user(**validated_data)
         return user
