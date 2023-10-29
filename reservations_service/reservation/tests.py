0c666c9bed908031f5a464c32e2e466fe755781f
vietman2 <vietman2@DESKTOP-AN0QL3G.localdomain>
2023-10-29 18:46:39 +0900

    dummy for test

--- reservations_service/reservation/tests.py
+++ reservations_service/reservation/tests.py
@@ -3,3 +3,6 @@ from django.test import TestCase
 class TestRating(TestCase):
     def test_rating(self):
         self.assertEqual(1, 1)
+
+    def test_rating2(self):
+        self.assertEqual(1, 1)
