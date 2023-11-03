bdc6ee2d56657260ba775737eeb2e07af078baeb
vietman2 <vietman0@gmail.com>
2023-11-04 01:45:02 +0900

    migrations

--- user_management_service/coupon/models.py
+++ user_management_service/coupon/models.py
@@ -4,7 +4,7 @@ class Coupon(models.Model):
     coupon_id = models.AutoField(primary_key=True)
     coupon_name = models.CharField(max_length=100)
     coupon_description = models.CharField(max_length=100)
-    coupon_image = models.ImageField(upload_to='coupon_images')
+    #coupon_image = models.ImageField(upload_to='coupon_images')
     coupon_code = models.CharField(max_length=100)
     coupon_discount = models.FloatField()
     coupon_start_date = models.DateTimeField()
