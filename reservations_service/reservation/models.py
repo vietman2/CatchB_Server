adb44e2650234d0e82731a5bb01c58e89faf1383
vietman2 <vietman0@gmail.com>
2023-11-04 01:45:49 +0900

    migrations

--- reservations_service/reservation/models.py
+++ reservations_service/reservation/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class StatusChoices(models.TextChoices):
+    WAITING     = "WAITING", "대기"
+    CONFIRMED   = "CONFIRMED", "승인"
+    CANCELLED   = "CANCELLED", "취소"
+    FINISHED    = "FINISHED", "완료"
+
 class Reservation(models.Model):
     facility        = models.UUIDField(null=False, blank=False, db_comment="시설 고유번호")
     reserved_user   = models.UUIDField(null=False, blank=False, db_comment="사용자 고유번호")
@@ -12,7 +18,12 @@ class Reservation(models.Model):
 
     start_time      = models.DateTimeField(null=False, blank=False, db_comment="시작 시간")
     end_time        = models.DateTimeField(null=False, blank=False, db_comment="종료 시간")
-    is_confirmed    = models.BooleanField(default=False, db_comment="예약 승인 여부")
+    status          = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.WAITING,
+        db_comment="예약 상태"
+    )
 
     class Meta:
         db_table = "reservation"
