0678c6c7bdb4a2a30efd7a7d6b502eaa1035a852
vietman2 <vietman0@gmail.com>
2023-11-04 01:46:57 +0900

    schedule app and model

---
+++ reservations_service/schedule/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Schedule(models.Model):
+    user_id = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_id
---
+++ reservations_service/schedule/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class ScheduleTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_schedule(self):
+        pass
