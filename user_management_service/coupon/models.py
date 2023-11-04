98176ad0dcf2defb41c5db126e6bafd99f1573e3
vietman2 <vietman0@gmail.com>
2023-11-04 22:42:57 +0900

    finish coupon and point models

--- user_management_service/coupon/models.py
+++ user_management_service/coupon/models.py
@@ -1,17 +1,45 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class CouponStatus(models.TextChoices):
+    ACTIVE      = 'ACTIVE', "사용 가능"
+    USED        = 'USED', "사용 완료"
+    EXPIRED     = 'EXPIRED', "쿠폰 만료"
 
 class Coupon(models.Model):
-    coupon_id = models.AutoField(primary_key=True)
+    user        = models.ForeignKey(
+        'user.CustomUser',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='user_coupons'
+    )
+    coupon_type = models.ForeignKey(
+        'coupon.CouponType',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='coupon_type'
+    )
+    issued_at   = models.DateTimeField(auto_now_add=True)
+    status      = models.CharField(
+        max_length=10,
+        choices=CouponStatus.choices,
+        default=CouponStatus.ACTIVE
+    )
+    valid_until = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'coupon'
+        verbose_name = _('coupon')
+        verbose_name_plural = _('coupons')
+
+class CouponType(models.Model):
     coupon_name = models.CharField(max_length=100)
     coupon_description = models.CharField(max_length=100)
     #coupon_image = models.ImageField(upload_to='coupon_images')
-    coupon_code = models.CharField(max_length=100)
-    coupon_discount = models.FloatField()
-    coupon_start_date = models.DateTimeField()
-    coupon_end_date = models.DateTimeField()
-    coupon_created_date = models.DateTimeField(auto_now_add=True)
-    coupon_updated_date = models.DateTimeField(auto_now=True)
-    coupon_is_active = models.BooleanField(default=True)
+    registered_at = models.DateTimeField(auto_now_add=True)
+    valid_days = models.IntegerField()
 
-    def __str__(self):
-        return self.coupon_name
+    class Meta:
+        db_table = 'coupon_type'
+        verbose_name = _('coupon_type')
+        verbose_name_plural = _('coupon_types')
--- user_management_service/point/models.py
+++ user_management_service/point/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class PointStatus(models.TextChoices):
+    ACTIVE      = 'ACTIVE', "사용 가능"
+    USED        = 'USED', "사용 완료"
+    EXPIRED     = 'EXPIRED', "쿠폰 만료"
 
 class Points(models.Model):
-    user_id = models.CharField(max_length=200)
-    point = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    user        = models.ForeignKey(
+        'user.CustomUser',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='user_points'
+    )
+    point       = models.IntegerField(default=0)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    status      = models.CharField(
+        max_length=10,
+        choices=PointStatus.choices,
+        default=PointStatus.ACTIVE
+    )
+    valid_until = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'points'
+        verbose_name = _('points')
+        verbose_name_plural = _('points')
