8558e8bd75a5d3ffc54ee67b44cee759259c3ed2
vietman2 <vietman0@gmail.com>
2023-11-16 20:30:40 +0900

    refactor

--- calendar_service/calendar_service/wsgi.py
+++ calendar_service/calendar_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for reservations_service project.
+WSGI config for calendar_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reservations_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendar_service.settings')
 
 application = get_wsgi_application()
