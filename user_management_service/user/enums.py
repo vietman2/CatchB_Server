bbfa8bf6843e328b615a83a521254c0fbf09b399
vietman2 <vietman0@gmail.com>
2023-12-28 15:19:49 +0900

    add undefined to both user enums

--- user_management_service/user/enums.py
+++ user_management_service/user/enums.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class ExperienceTierChoices(models.IntegerChoices):
-    BEGINNER = 0, '초보'
-    INTERMEDIATE = 1, '중급'
-    ADVANCED = 2, '고급'
+    UNDEFINED = 0, '미입력'
+    BEGINNER = 1, '초보'
+    INTERMEDIATE = 2, '중급'
+    ADVANCED = 3, '고급'
 
 class RegisterRouteChoices(models.IntegerChoices):
-    CATCHB = 0, '캐치비'
-    KAKAO = 1, '카카오'
-    NAVER = 2, '네이버'
+    UNDEFINED = 0, '미입력'
+    CATCHB = 1, '캐치비'
+    KAKAO = 2, '카카오'
+    NAVER = 3, '네이버'
