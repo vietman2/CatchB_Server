d3ba5bb7b9a22b00dd9eafc9ac16d34196ff486f
vietman2 <vietman0@gmail.com>
2024-01-04 09:25:13 +0900

    try switching fbv to cbv

--- api_gateway/users/urls.py
+++ api_gateway/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
     path('token/refresh/', views.refresh_token, name='refresh_token'),
     path('<str:uuid>/', views.get_user_profile, name='get_user_profile')
