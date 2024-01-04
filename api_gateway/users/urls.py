d9c3efa2a430591a4eec28f15460a73ffc70fe17
vietman2 <vietman0@gmail.com>
2024-01-04 09:54:42 +0900

    switch all to cbv

--- api_gateway/users/urls.py
+++ api_gateway/users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.UserView.as_view(), name='users'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('token/refresh/', views.refresh_token, name='refresh_token'),
-    path('<str:uuid>/', views.get_user_profile, name='get_user_profile')
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('token/refresh/', views.TokenView.as_view(), name='refresh_token'),
 ]
--- api_gateway/users/views.py
+++ api_gateway/users/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 
 user_service_url = settings.SERVICE_URLS['user_management_service']
 
@@ -27,26 +27,27 @@ def get_response(request, url, method):
             status=status.HTTP_502_BAD_GATEWAY,
         )
 
-@api_view(['POST'])
-def refresh_token(request):
-    REQUEST_URL = f'{user_service_url}/api/token/refresh/'
-
-    return get_response(request, REQUEST_URL, 'POST')
-
-@api_view(['GET'])
-def get_user_profile(request, uuid):
-    REQUEST_URL = f'{user_service_url}/api/users/{uuid}/'
-
-    return get_response(request, REQUEST_URL, 'GET')
-
-@api_view(['POST'])
-def logout(request):
-    REQUEST_URL = f'{user_service_url}/api/logout/'
-
-    return get_response(request, REQUEST_URL, 'POST')
-
 class LoginView(APIView):
     def post(self, request):
         REQUEST_URL = f'{user_service_url}/api/login/'
+
+        return get_response(request, REQUEST_URL, 'POST')
+
+class LogoutView(APIView):
+    def post(self, request):
+        REQUEST_URL = f'{user_service_url}/api/logout/'
+
+        return get_response(request, REQUEST_URL, 'POST')
+
+class UserView(APIView):
+    def get(self, request, *args, **kwargs):
+        uuid = request.query_params.get('uuid', None)
+        REQUEST_URL = f'{user_service_url}/api/users/{uuid}/'
+
+        return get_response(request, REQUEST_URL, 'GET')
+
+class TokenView(APIView):
+    def post(self, request):
+        REQUEST_URL = f'{user_service_url}/api/token/refresh/'
 
         return get_response(request, REQUEST_URL, 'POST')
