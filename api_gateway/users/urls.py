dd30fe058711af8c1e4f00641f2ca6ff763088cb
vietman2 <vietman0@gmail.com>
2024-01-09 15:05:57 +0900

    route coupon register

--- api_gateway/users/urls.py
+++ api_gateway/users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('token/refresh/', views.TokenView.as_view(), name='refresh_token'),
+    path('coupons/register/', views.CouponRegisterView.as_view(), name='coupons'),
+    path('coupons/status/', views.CouponStatusCheckView.as_view(), name='coupons_status'),
 ]
