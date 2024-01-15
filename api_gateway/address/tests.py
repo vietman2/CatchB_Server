de41cd82845493d40ff4795407e64c900df241ce
vietman2 <vietman0@gmail.com>
2024-01-16 06:03:47 +0900

    placeholders for tests

--- api_gateway/address/tests.py
+++ api_gateway/address/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+class PlaceHolderTest(TestCase):
+    def test_placeholder(self):
+        self.assertEqual(1, 1)
--- api_gateway/users/tests.py
+++ api_gateway/users/tests.py
@@ -3,6 +3,3 @@ from django.test import TestCase
 class TestPlaceholder(TestCase):
     def test_placeholder(self):
         self.assertEqual(1, 1)
-
-    def test_workflow(self):
-        self.assertEqual(2, 2)
