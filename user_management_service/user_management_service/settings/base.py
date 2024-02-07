96f80a84dca043552349ede5ea0ab4dc0b0250ae
vietman2 <vietman0@gmail.com>
2024-02-07 15:45:40 +0900

    customize token

--- user_management_service/user_management_service/settings/base.py
+++ user_management_service/user_management_service/settings/base.py
@@ -125,6 +125,7 @@ SIMPLE_JWT = {
     'USER_ID_CLAIM': 'user_id',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_OBTAIN_PAIR_SERIALIZER': 'user.serializers.MyTokenObtainPairSerializer',
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
