a818de377d325e941425b67b1d5a3c7c38311336
vietman2 <vietman0@gmail.com>
2024-02-18 19:20:41 +0900

    refactor api urls

--- products_service/products_service/urls.py
+++ products_service/products_service/urls.py
@@ -15,25 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView as SSV,
 )
 
-from facility.views import FacilityViewSet      ## pylint: disable=no-name-in-module
-from lesson.views import LessonProductViewSet
-from region.views import RegionViewSet
-
-router = DefaultRouter()
-
-router.register(r'facilities', FacilityViewSet)
-router.register(r'lessons', LessonProductViewSet)
-router.register(r'regions', RegionViewSet)
-
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('api/', include('core.api_urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SSV.as_view(url_name='schema'), name='swagger-ui'),
