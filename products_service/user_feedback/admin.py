e42f80cdc1e98791ff112419ef189b0b7def4555
vietman2 <vietman0@gmail.com>
2024-02-18 19:16:43 +0900

    remove user_feedback app: to be refactored later

--- products_service/user_feedback/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import CoachReview, CoachLike, FacilityReview, FacilityLike
-
-admin.register(CoachReview)
-admin.register(CoachLike)
-admin.register(FacilityReview)
-admin.register(FacilityLike)
--- products_service/user_feedback/tests.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.test import APITestCase
-
-class Test(APITestCase):
-    def test(self):
-        self.assertEqual(1, 1)
