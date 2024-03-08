68303f0480856c2d433988945b6b7e3101d2c657
vietman2 <vietman0@gmail.com>
2024-03-08 10:00:18 +0900

    fix import

--- community_service/board/models.py
+++ community_service/board/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from ..core.models import TimeStampedModel
+from core.models import TimeStampedModel
 from .enums import ForumChoices
 
 class Post(TimeStampedModel):
