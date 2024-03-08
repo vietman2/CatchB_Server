382d791d3a2fc4dd900bf08f6f95a144f06ce8f9
vietman2 <vietman0@gmail.com>
2024-03-08 22:45:18 +0900

    add like and report abstract model

--- community_service/core/models.py
+++ community_service/core/models.py
@@ -1,11 +1,36 @@
 from django.db import models
 
+from .enums import ReportReason
+
 class TimeStampedModel(models.Model):
     created_at      = models.DateTimeField(auto_now_add=True)
     updated_at      = models.DateTimeField(auto_now=True)
 
     is_deleted      = models.BooleanField(default=False)
     deleted_at      = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+class Like(models.Model):
+    user_uuid       = models.UUIDField(editable=False)
+
+    liked_at        = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Report(TimeStampedModel):
+    report_user_uuid    = models.UUIDField(editable=False)
+    report_content      = models.TextField()
+    report_reason       = models.CharField(
+        max_length=2,
+        choices=ReportReason.choices,
+        default=ReportReason.OTHER
+    )
+
+    reviewed            = models.BooleanField(default=False)
+    feedback            = models.TextField(null=True)
 
     class Meta:
         abstract = True
