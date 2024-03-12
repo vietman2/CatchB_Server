2f37f3b5c618175cdc57f8bcdbf8c44b4d0e8a46
vietman2 <vietman0@gmail.com>
2024-03-12 11:47:32 +0900

    add imageview url

--- community_service/core/api_urls.py
+++ community_service/core/api_urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from post.views import TagViewSet
+from post.views import TagViewSet, ImageViewSet
 
 router = DefaultRouter()
 
 router.register(r'tags', TagViewSet, basename='tag')
+router.register(r'images', ImageViewSet, basename='image')
 
 urlpatterns = router.urls
