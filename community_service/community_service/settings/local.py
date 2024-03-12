b7397545f165eecc0f355f8465b8b93f894e789d
vietman2 <vietman0@gmail.com>
2024-03-12 11:46:52 +0900

    configure storage

--- community_service/community_service/settings/local.py
+++ community_service/community_service/settings/local.py
@@ -23,3 +23,15 @@ DATABASES = {
     #    'PORT': '5433',
     #}
 }
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = 'catchb.media'
+AWS_S3_ENDPOINT_URL = 'https://kr.object.ncloudstorage.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    'ACL': 'public-read',
+}
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = 'https://kr.object.ncloudstorage.com/catchb.media/'
