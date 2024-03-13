cbab7a714f270e443c10a0c1ffb2154c32e0c53b
vietman2 <vietman0@gmail.com>
2024-03-13 15:48:05 +0900

    route posts api

--- community_service/core/api_urls.py
+++ community_service/core/api_urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from post.views import TagViewSet, ImageViewSet
+from post.views import TagViewSet, ImageViewSet, PostViewSet
 
 router = DefaultRouter()
 
 router.register(r'tags', TagViewSet, basename='tag')
 router.register(r'images', ImageViewSet, basename='image')
+router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = router.urls
