dde03ad91ba028737e8af347e9a37be467b3e4bc
vietman2 <vietman0@gmail.com>
2024-03-20 16:50:14 +0900

    route coach apis and facility status api

--- api_gateway/products/urls.py
+++ api_gateway/products/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('facilities/', views.FacilityView.as_view(), name='address'),
-    path('facilities/<str:uuid>/', views.FacilityInfoView.as_view(), name='info'),
-    path('regions/', views.RegionView.as_view(), name='regions'),
+    path('coaches/', views.CoachView.as_view(), name='coach'),
+    path('coaches/status/', views.CoachStatusView.as_view(), name='status'),
+    path('coaches/<str:uuid>/', views.CoachInfoView.as_view(), name='coach_info'),
+    path('facilities/', views.FacilityView.as_view(), name='facility'),
+    path('facilities/status/', views.FacilityStatusView.as_view(), name='status'),
+    path('facilities/<str:uuid>/', views.FacilityInfoView.as_view(), name='facility_info'),
+    path('regions/', views.RegionView.as_view(), name='region'),
 ]
--- api_gateway/products/views.py
+++ api_gateway/products/views.py
@@ -8,6 +8,68 @@ from core.views import get_response
 
 products_service_url = settings.SERVICE_URLS['products_service']
 
+class CoachView(APIView):
+    def post(self, request):
+        if not IsLoggedIn().has_permission(request, self):
+            return Response(
+                {'message': '로그인이 필요합니다.'},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        REQUEST_URL = f'{products_service_url}/api/coaches/'
+
+        return get_response(request.headers, request.body, REQUEST_URL, 'POST')
+
+    def get(self, request):
+        REQUEST_URL = f'{products_service_url}/api/coaches/'
+
+        return get_response(request.headers, request.body, REQUEST_URL, 'GET')
+
+class CoachInfoView(APIView):
+    def post(self, request, uuid):
+        if not IsLoggedIn().has_permission(request, self):
+            return Response(
+                {'message': '로그인이 필요합니다.'},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        REQUEST_URL = f'{products_service_url}/api/coaches/{uuid}/info/'
+
+        return get_response(
+            request.headers,
+            request.body,
+            REQUEST_URL,
+            'POST'
+        )
+
+    def get(self, request, uuid):
+        REQUEST_URL = f'{products_service_url}/api/coaches/{uuid}/'
+
+        return get_response(
+            request.headers,
+            request.body,
+            REQUEST_URL,
+            'GET'
+        )
+
+class CoachStatusView(APIView):
+    def get(self, request):
+        if not IsLoggedIn().has_permission(request, self):
+            return Response(
+                {'message': '로그인이 필요합니다.'},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        REQUEST_URL = f'{products_service_url}/api/coaches/status/'
+
+        return get_response(
+            request.headers,
+            request.body,
+            REQUEST_URL,
+            'GET',
+            request.query_params
+        )
+
 class FacilityView(APIView):
     def post(self, request):
         if not IsLoggedIn().has_permission(request, self):
@@ -40,6 +102,34 @@ class FacilityInfoView(APIView):
             request.body,
             REQUEST_URL,
             'POST'
+        )
+
+    def get(self, request, uuid):
+        REQUEST_URL = f'{products_service_url}/api/facilities/{uuid}/'
+
+        return get_response(
+            request.headers,
+            request.body,
+            REQUEST_URL,
+            'GET'
+        )
+
+class FacilityStatusView(APIView):
+    def get(self, request):
+        if not IsLoggedIn().has_permission(request, self):
+            return Response(
+                {'message': '로그인이 필요합니다.'},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        REQUEST_URL = f'{products_service_url}/api/facilities/status/'
+
+        return get_response(
+            request.headers,
+            request.body,
+            REQUEST_URL,
+            'GET',
+            request.query_params
         )
 
 class RegionView(APIView):
