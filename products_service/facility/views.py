9b684f759fed2009c36e07373ce5ff8bcdcb0d3b
vietman2 <vietman0@gmail.com>
2024-03-20 16:49:23 +0900

    add facility register status api

--- products_service/facility/views.py
+++ products_service/facility/views.py
@@ -2,6 +2,7 @@ import requests
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.base import ContentFile
+from django.db.models import Q
 from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
@@ -14,7 +15,7 @@ from region.models import Sigungu
 from .models import Facility, FacilityInfo
 from .serializers import (
     FacilitySimpleSerializer, FacilityCreateSerializer,
-    FacilityInfoCreateSerializer
+    FacilityInfoCreateSerializer, FacilityDetailSeralizer
 )
 
 def get_coordinates(address):
@@ -110,9 +111,19 @@ class FacilityViewSet(ModelViewSet):
     serializer_class = FacilitySimpleSerializer
     http_method_names = ["get", "post"]
 
+    @extend_schema(summary="시설 상세 조회", tags=["시설"])
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = FacilityDetailSeralizer(instance)
+        return Response(serializer.data)
+
     @extend_schema(summary="시설 목록 조회", tags=["시설"])
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        queryset = self.get_queryset()
+        ## TODO: Facility.objects.get(is_confirmed=True)
+        ## TODO: pagination, filter based on lat and lng
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     @extend_schema(summary="시설 등록 신청", tags=["시설"])
     def create(self, request, *args, **kwargs):     ## pylint: disable=R0914, W0613
@@ -135,10 +146,10 @@ class FacilityViewSet(ModelViewSet):
             data['jibun_address'] = jibun_address
             data['eng_address'] = english_address
 
-            facility_serializer = FacilityCreateSerializer(data=data)
-            facility_serializer.is_valid(raise_exception=True)
+            serializer = FacilityCreateSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
 
-            facility = facility_serializer.save()
+            facility = serializer.save()
 
             image = fetch_map_image(facility.latitude, facility.longitude)
             path = f"products/facility/{facility.uuid}/map_image.png"
@@ -160,27 +171,93 @@ class FacilityViewSet(ModelViewSet):
     @extend_schema(summary="시설 정보 입력", tags=["시설"])
     def info(self, request, *args, **kwargs):       ## pylint: disable=W0613
         facility = self.get_object()
-        facility_info_serializer = FacilityInfoCreateSerializer(data=request.data)
+        serializer = FacilityInfoCreateSerializer(data=request.data)
 
         try:
             if FacilityInfo.objects.filter(facility=facility).exists():
                 raise ValidationError("이미 등록된 시설 정보입니다.")
-            facility_info_serializer.is_valid(raise_exception=True)
 
-            facility_info_serializer.convenience(request.POST.getlist('convenience'))
-            facility_info_serializer.equipment(request.POST.getlist('equipment'))
-            facility_info_serializer.others(request.POST.getlist('others'))
-            #facility_info_serializer.custom_equipment(request.POST.getlist('custom'))
-            #facility_info_serializer.upload_images(request.FILES.getlist('images'), facility.uuid)
+            serializer.is_valid(raise_exception=True)
 
-            facility_info_serializer.validated_data['facility'] = facility
+            serializer.convenience(request.POST.getlist('convenience'))
+            serializer.equipment(request.POST.getlist('equipment'))
+            serializer.others(request.POST.getlist('others'))
+            #serializer.custom_equipment(request.POST.getlist('custom'))
+            #serializer.upload_images(request.FILES.getlist('images'), facility.uuid)
 
-            facility_info_serializer.save()
+            serializer.validated_data['facility'] = facility
+
+            serializer.save()
+
+            return Response(
+                status=status.HTTP_201_CREATED,
+                data={"message": "시설 정보 입력이 완료되었습니다."}
+            )
 
         except ValidationError as e:
             return get_error_message(e)
 
+    @action(detail=False, methods=["get"])
+    @extend_schema(summary="시설 등록 현황 조회", tags=["시설"])
+    def status(self, request, *args, **kwargs):
+        if "uuid" in request.query_params:
+            user_uuid = request.query_params["uuid"]
+        else:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"message": "잘못된 요청입니다."}
+            )
+        
+        q = Q()
+        q &= Q(member_uuid=user_uuid)
+
+        facilities = Facility.objects.filter(q)
+
+        ## 0. 등록한 시설이 없음
+        if not facilities.exists():
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "step": 0,
+                    "title": "등록 시작",
+                    "message": "사장님의 첫번째 아카데미를 등록해 보세요!"
+                }
+            )
+
+        q &= Q(is_complete=False)
+        ## 1. if 등록 절차가 안끝난 아카데미가 없는 경우: 새로운 아카데미 등록
+        if not facilities.filter(q).exists():
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "step": 0,
+                    "title": "새로 등록하기",
+                    "message": "이미 운영중인 아카데미가 있습니다.\n새로운 아카데미를 등록하시겠습니까?"
+                }
+            )
+
+        ## 2. if 시설 정보 입력이 안끝난 경우: step 1
+        facility = facilities.filter(q).first()
+        if not FacilityInfo.objects.filter(facility=facility).exists():
+            step = 1
+        ## TODO: Finish this
+        #elif facility.bank is None:
+        #    step = 2
+        #elif facility.products.count() == 0:
+        #    step = 3
+        else:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"message": "오류가 발생했습니다. 나중에 다시 시도해주세요."}
+            )
+
         return Response(
-            status=status.HTTP_201_CREATED,
-            data={"message": "시설 정보 입력이 완료되었습니다."}
+            status=status.HTTP_200_OK,
+            data={
+                "facility": facility.uuid,
+                "step": step,
+                "status": facility.is_confirmed,
+                "title": "이어서 등록하기",
+                "message": "이미 등록중인 아카데미가 있습니다.\n아카데미 정보를 이어서 입력해주세요."
+            }
         )
