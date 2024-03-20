caa088038ec9d16c355ba0112c5e0da7225d8cc7
vietman2 <vietman0@gmail.com>
2024-03-20 16:47:49 +0900

    route coach viewset

--- products_service/core/api_urls.py
+++ products_service/core/api_urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
 
+from coach.views import CoachViewSet
 from facility.views import FacilityViewSet      ## pylint: disable=no-name-in-module
 from lesson.views import LessonProductViewSet
 from region.views import RegionViewSet
 
 router = DefaultRouter()
 
+router.register(r'coaches', CoachViewSet, basename='coach')
 router.register(r'facilities', FacilityViewSet, basename='facility')
 router.register(r'lessons', LessonProductViewSet, basename='lesson')
 router.register(r'regions', RegionViewSet, basename='region')
