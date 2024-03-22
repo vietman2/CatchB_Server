adfec789c50ab98c45adff9936b6a625a1b3429c
vietman2 <vietman0@gmail.com>
2024-03-22 14:20:10 +0900

    lint fixes

--- products_service/coach/tests.py
+++ products_service/coach/tests.py
@@ -19,7 +19,7 @@ class CoachCreateAPITestCase(APITestCase):
     def setUp(self):
         self.url = "/api/coaches/"
         self.data = {
-            "member_uuid": "7e1186bf-3172-41df-91fd-a9f74f2508ca",
+            "member_uuid": "e8217f3c-1aee-4a24-9fe6-a1347b278c16",
             "member_name": "홍길동",
             "member_phone": "010-1234-5678",
             "baseball_career": "고등학교 선수 출신",
@@ -28,26 +28,42 @@ class CoachCreateAPITestCase(APITestCase):
     @patch("django.core.files.storage.default_storage.save")
     def test_coach_create_pdf(self, mock_save):
         mock_save.return_value = "test.png"
-        self.data["certification"] = SimpleUploadedFile("test.pdf", b"file_content", content_type="application/pdf")
+        self.data["certification"] = SimpleUploadedFile(
+            "test.pdf",
+            b"file_content",
+            content_type="application/pdf"
+        )
         response = self.client.post(self.url, self.data, format="multipart")
         self.assertEqual(response.status_code, 201)
 
     @patch("django.core.files.storage.default_storage.save")
     def test_coach_create_png(self, mock_save):
         mock_save.return_value = "test.png"
-        self.data["certification"] = SimpleUploadedFile("test.png", b"file_content", content_type="image/png")
+        self.data["certification"] = SimpleUploadedFile(
+            "test.png",
+            b"file_content",
+            content_type="image/png"
+        )
         response = self.client.post(self.url, self.data, format="multipart")
         self.assertEqual(response.status_code, 201)
 
     @patch("django.core.files.storage.default_storage.save")
     def test_coach_create_jpeg(self, mock_save):
         mock_save.return_value = "test.png"
-        self.data["certification"] = SimpleUploadedFile("test.jpg", b"file_content", content_type="image/jpeg")
+        self.data["certification"] = SimpleUploadedFile(
+            "test.jpg",
+            b"file_content",
+            content_type="image/jpeg"
+        )
         response = self.client.post(self.url, self.data, format="multipart")
         self.assertEqual(response.status_code, 201)
 
     def test_coach_create_invalid_certification(self):
-        self.data["certification"] = SimpleUploadedFile("test.txt", b"file_content", content_type="text/plain")
+        self.data["certification"] = SimpleUploadedFile(
+            "test.txt",
+            b"file_content",
+            content_type="text/plain"
+        )
         response = self.client.post(self.url, self.data, format="multipart")
         self.assertEqual(response.status_code, 400)
 
@@ -60,7 +76,7 @@ class CoachInfoCreateAPITestCase(APITestCase):
     fixtures = ["init_data.json", "test_data.json"]
 
     def setUp(self):
-        self.uuid = "7e1186bf-3172-41df-91fd-a9f74f2508ca"
+        self.uuid = "c5c5636e-74e8-4ee5-b0d8-8356830b2e76"
         self.url = f"/api/coaches/{self.uuid}/info/"
         self.data = {
             "intro": "투수로서의 경험을 공유합니다.",
