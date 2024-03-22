27620b270bd6f06241d335a18915c5fe14944468
vietman2 <vietman0@gmail.com>
2024-03-22 22:34:11 +0900

    add tests and raise coverage

--- community_service/post/tests.py
+++ community_service/post/tests.py
@@ -2,15 +2,17 @@ from unittest.mock import patch
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework.test import APITestCase
 
-from .models import Tag, Post
+from .models import Post
 
 class TagAPITest(APITestCase):
-    def setUp(self):
-        self.url = '/api/tags/'
+    fixtures = ['init_data.json']
 
     def test_list(self):
-        response = self.client.get(self.url)
+        response = self.client.get('/api/tags/')
         self.assertEqual(response.status_code, 200)
+        
+        response = self.client.get('/api/tags/1/')
+        self.assertEqual(response.status_code, 405)
 
 class ImageUploadAPITest(APITestCase):
     def setUp(self):
@@ -28,6 +30,8 @@ class ImageUploadAPITest(APITestCase):
         self.assertEqual(response.status_code, 201)
 
 class PostAPITest(APITestCase):
+    fixtures = ['init_data.json', 'test_data.json']
+
     def setUp(self):
         self.url = '/api/posts/'
         self.data = {
@@ -37,16 +41,6 @@ class PostAPITest(APITestCase):
             'tags': [1, 2],
             'images': []
         }
-        Tag.objects.create(name='test1', icon='test1', color='test1', bgcolor='test1', forum=1)
-        Tag.objects.create(name='test2', icon='test2', color='test2', bgcolor='test2', forum=1)
-        Tag.objects.create(name='test3', icon='test3', color='test3', bgcolor='test3', forum=1)
-        Tag.objects.create(name='test4', icon='test4', color='test4', bgcolor='test4', forum=1)
-        self.post = Post.objects.create(
-            title='title',
-            content='content',
-            author_uuid='123e4567-e89b-12d3-a456-426614174000',
-            forum=1
-        )
 
     def test_create_success(self):
         self.data['forum'] = '덕아웃'
@@ -65,34 +59,46 @@ class PostAPITest(APITestCase):
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 201)
 
+    def test_create_primary_key(self):
+        ## delete all posts
+        Post.objects.all().delete()
+        self.data['forum'] = '덕아웃'
+        response = self.client.post(self.url, self.data, format='json')        
+
     def test_create_fail(self):
+        # 1. duplicate title in same forum
         self.data['forum'] = '덕아웃'
         self.client.post(self.url, self.data, format='json')
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 400)
 
+        # 2. no title
         self.data['title'] = ""
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 400)
 
+        # 3. too many tags
         self.data['title'] = "new title"
         self.data['tags'] = [1, 2, 3, 4]
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 400)
 
+        # 4. forum dne
         self.data['forum'] = '테스트'
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 400)
 
     def test_list(self):
-        query = '?forum=덕아웃'
-        response = self.client.get(self.url + query)
+        self.data['forum'] = '덕아웃'
+        self.client.post(self.url, self.data, format='json')
+        response = self.client.get(self.url, {'forum': '덕아웃'})
         self.assertEqual(response.status_code, 200)
 
     def test_list_fail(self):
+        # 1. no forum (query)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 400)
 
     def test_retrieve(self):
-        response = self.client.get(self.url + str(self.post.id) + '/')
+        response = self.client.get(self.url + '2024031500000001/')
         self.assertEqual(response.status_code, 200)
