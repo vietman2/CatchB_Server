b6ce6135a880f71beb65b988424b9ffa6becf25d
vietman2 <vietman0@gmail.com>
2024-03-22 22:35:25 +0900

    refactor 2 methods

--- products_service/facility/views.py
+++ products_service/facility/views.py
@@ -1,7 +1,5 @@
 import requests
-from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist, ValidationError as DjValidError
-from django.core.files.base import ContentFile
 from django.db.models import Q
 from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import status
@@ -11,38 +9,13 @@ from rest_framework.serializers import ValidationError
 from rest_framework.decorators import action
 from drf_spectacular.utils import extend_schema
 
+from core.utils import get_coordinates, fetch_map_image
 from region.models import Sigungu
 from .models import Facility, FacilityInfo
 from .serializers import (
     FacilitySimpleSerializer, FacilityCreateSerializer,
     FacilityInfoCreateSerializer, FacilityInfoDetailSerializer
 )
-
-def get_coordinates(address):
-    naver_geocode_url = 'https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode'
-    headers = {
-        'X-NCP-APIGW-API-KEY-ID': settings.NAVER_CLIENT_ID,
-        'X-NCP-APIGW-API-KEY': settings.NAVER_CLIENT_SECRET,
-        'Accept': 'application/json',
-    }
-    params = {
-        'query': address,
-    }
-
-    response = requests.request(
-            method='GET',
-            url=naver_geocode_url,
-            headers=headers,
-            params=params,
-            timeout=10,
-        )
-
-    lat = response.json()['addresses'][0]['y']
-    lng = response.json()['addresses'][0]['x']
-    jibun_address = response.json()['addresses'][0]['jibunAddress']
-    english_address = response.json()['addresses'][0]['englishAddress']
-
-    return lat, lng, jibun_address, english_address
 
 def get_error_message(e):
     err_types = ["name", "phone", "reg_code", "num_mounds", "num_plates",
@@ -66,31 +39,6 @@ def get_error_message(e):
         status=status.HTTP_400_BAD_REQUEST,
         data={"message": "등록 실패했습니다."}
     )
-
-def fetch_map_image(lat, lng):
-    naver_staticmap_url = 'https://naveropenapi.apigw.ntruss.com/map-static/v2/raster'
-    headers = {
-        'X-NCP-APIGW-API-KEY-ID': settings.NAVER_CLIENT_ID,
-        'X-NCP-APIGW-API-KEY': settings.NAVER_CLIENT_SECRET,
-    }
-    params = {
-        'center': f'{lng},{lat}',
-        'level': 15,
-        'w': 500,
-        'h': 300,
-        'format': 'png',
-        'markers': f'type:d|size:small|pos:{lng} {lat}|color:0x14863e|viewSizeRatio:0.75',
-    }
-
-    response = requests.request(
-            method='GET',
-            url=naver_staticmap_url,
-            headers=headers,
-            params=params,
-            timeout=10,
-        )
-
-    return ContentFile(response.content)
 
 class FacilityViewSet(ModelViewSet):
     queryset = Facility.objects.all()
