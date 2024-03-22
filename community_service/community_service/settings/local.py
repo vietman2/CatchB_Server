99cdb0395e8d9eab5fcd9dbe7490b01e40f2751b
vietman2 <vietman0@gmail.com>
2024-03-22 22:31:38 +0900

    Remove public read option

--- community_service/community_service/settings/local.py
+++ community_service/community_service/settings/local.py
@@ -30,7 +30,6 @@ AWS_STORAGE_BUCKET_NAME = 'catchb.media'
 AWS_S3_ENDPOINT_URL = 'https://kr.object.ncloudstorage.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
-    'ACL': 'public-read',
 }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
