32f7e5388ea84ffc3b37b3203eac6345cadda6e7
vietman2 <vietman0@gmail.com>
2024-03-25 17:03:30 +0900

    route post like and dislike

--- api_gateway/community/urls.py
+++ api_gateway/community/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("images/", views.ImageView.as_view(), name="images"),
     path("posts/", views.PostView.as_view(), name="posts"),
     path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("posts/<int:pk>/like/", views.PostLikeView.as_view(), name="post_like"),
+    path("posts/<int:pk>/dislike/", views.PostDislikeView.as_view(), name="post_dislike"),
     path("comments/", views.CommentView.as_view(), name="comments"),
 ]
