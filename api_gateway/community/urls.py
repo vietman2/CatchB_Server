1ea51f02cb0ef8ae7d71724071d44a689d0f6d19
vietman2 <vietman0@gmail.com>
2024-03-25 13:44:14 +0900

    route comment

--- api_gateway/community/urls.py
+++ api_gateway/community/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("images/", views.ImageView.as_view(), name="images"),
     path("posts/", views.PostView.as_view(), name="posts"),
     path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("comments/", views.CommentView.as_view(), name="comments"),
 ]
