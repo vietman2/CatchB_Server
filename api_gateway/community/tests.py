dfc73c4e63fbd17a57f354ef128a825ecdd53f24
vietman2 <vietman0@gmail.com>
2024-03-28 15:05:08 +0900

    add tests

--- api_gateway/community/tests.py
+++ api_gateway/community/tests.py
@@ -145,3 +145,61 @@ class CommentAPITests(APITestCase):
             status_code=201
         )
         self.client.post('/api/community/comments/', {'content': 'content'})
+
+    @requests_mock.Mocker()
+    def test_comment_list(self, m):
+        m.get(
+            self.community_server + '/api/comments/',
+            json=[{'id': 1, 'content': 'content'}],
+            status_code=200
+        )
+        self.client.get('/api/community/comments/')
+
+    @requests_mock.Mocker()
+    def test_comment_list_logged_in(self, m):
+        with patch("community.views.get_user_info") as mock_get_user_info, \
+             patch("core.permissions.IsLoggedIn.has_permission") as mock_has_permission:
+            mock_get_user_info.return_value = {'user_id': '1'}
+            mock_has_permission.return_value = True
+            m.get(
+                self.community_server + '/api/comments/',
+                json=[{'id': 1, 'content': 'content'}],
+                status_code=200
+            )
+            self.client.get('/api/community/comments/')
+
+class CommentLikeDislikeAPITests(APITestCase):
+    def setUp(self):
+        self.community_server = 'http://localhost:8002'
+
+    @requests_mock.Mocker()
+    @patch("core.permissions.get_user_info")
+    def test_comment_like(self, m, mock_get_user_info):
+        # 1: Fail: 401 Unauthorized
+        mock_get_user_info.return_value = None
+        self.client.post('/api/community/comments/1/like/')
+
+        # 2: Success
+        mock_get_user_info.return_value = {'user_id': '1'}
+        m.post(
+            self.community_server + '/api/comments/1/like/',
+            json={'id': 1},
+            status_code=201
+        )
+        self.client.post('/api/community/comments/1/like/')
+
+    @requests_mock.Mocker()
+    @patch("core.permissions.get_user_info")
+    def test_comment_dislike(self, m, mock_get_user_info):
+        # 1: Fail: 401 Unauthorized
+        mock_get_user_info.return_value = None
+        self.client.post('/api/community/comments/1/dislike/')
+
+        # 2: Success
+        mock_get_user_info.return_value = {'user_id': '1'}
+        m.post(
+            self.community_server + '/api/comments/1/dislike/',
+            json={'id': 1},
+            status_code=201
+        )
+        self.client.post('/api/community/comments/1/dislike/')
